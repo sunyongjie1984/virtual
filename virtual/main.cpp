5283d1a570a65a32e3707586a1a459dee1dfa574
yongjies <yongjies@cadence.com>
2022-05-08 23:47:52 +0800

    modify

--- virtual/main.cpp
+++ virtual/main.cpp
@@ -1,14 +1,53 @@
-//
-//  main.cpp
-//  virtual
-//
-//  Created by 孙永杰 on 2022/5/5.
-//
-
 #include <iostream>
+class CBook {
+public:
+	CBook()
+    {
+        i = 10;
+        j = 20;
+    }
+	int GetJ()              // 这样定义表示“命令”子类继承这个函数，没有什么说的。
+    {
+        return j;
+    }
+    virtual int GetIJ()     // 这样定义表示“期望”子类覆盖它，没有什么说的。
+    {
+        return i + j;
+    }
+	virtual int GetI() = 0; // 这样定义表示“命令”子类覆盖它，没有什么说的。
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+protected:
+	int i;
+	int j;
+};
+
+class CBookChild : public CBook {
+public:
+	int GetJ()  // 子类继承/重写了这个函数，没有听从我的命令。这是C++语言允许的的错误。
+    {
+        return j + 1;
+    }
+    int GetIJ() // 你覆盖了我，符合了我的期望，可是如果你没有覆盖我，我也不怪你。
+    {
+        return i + j + 1;
+    }
+	int GetI()  // 你覆盖了我，这是你的任务，听从了我的命令。你不覆盖我，那是c++语言不允许的错误。
+    {
+        return i + 1;
+    }
+};
+
+int main()
+{
+//  CBook* p = new CBook(); // 不能实例化对象
+	CBook* p = new CBookChild();
+	CBookChild* q = new CBookChild();
+	std::cout << p->GetJ()  << std::endl;
+    std::cout << p->GetIJ() << std::endl;
+	std::cout << p->GetI()  << std::endl;
+
+	std::cout << q->GetJ()  << std::endl;
+    std::cout << q->GetIJ() << std::endl;
+    std::cout << q->GetI()  << std::endl;
+	return 0;
 }
